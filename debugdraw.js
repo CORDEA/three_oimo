5df5bcbbc65f34d5dc227a3cc2cd8b556f39b59f
Yoshihiro Tanaka <contact@cordea.jp>
2022-08-25 08:42:05 +0900

    rename

--- debugdraw.js
+++ debugdraw.js
@@ -100,26 +100,26 @@ export class ThreeDebugDraw extends DebugDraw {
         this.group = new Group();
         this.group.add(points, lines, triangles);
         scene.add(this.group);
-        this.sizes = [0, 0, 0];
+        this.indices = [0, 0, 0];
     }
 
     point(v, c) {
         const mesh = this.group.getObjectByName('points');
-        let i = this.sizes[0];
+        let i = this.indices[0];
         const position = mesh.geometry.attributes.position;
         const color = mesh.geometry.attributes.color;
         position.setXYZ(i, v.x, v.y, v.z);
         color.setXYZ(i, c.x, c.y, c.z);
-        this.sizes[0] += 1;
-        if (this.sizes[0] !== bufferSize) {
+        this.indices[0] += 1;
+        if (this.indices[0] !== bufferSize) {
             return;
         }
-        this.sizes[0] = 0;
+        this.indices[0] = 0;
     }
 
     triangle(v1, v2, v3, n1, n2, n3, c) {
         const mesh = this.group.getObjectByName('triangles');
-        let i = this.sizes[1] * 3;
+        let i = this.indices[1] * 3;
         const position = mesh.geometry.attributes.position;
         const normal = mesh.geometry.attributes.normal;
         const color = mesh.geometry.attributes.color;
@@ -134,16 +134,16 @@ export class ThreeDebugDraw extends DebugDraw {
         position.setXYZ(i, v3.x, v3.y, v3.z);
         normal.setXYZ(i, n3.x, n3.y, n3.z);
         color.setXYZ(i, c.x, c.y, c.z);
-        this.sizes[1] += 1;
-        if (this.sizes[1] !== bufferSize) {
+        this.indices[1] += 1;
+        if (this.indices[1] !== bufferSize) {
             return;
         }
-        this.sizes[1] = 0;
+        this.indices[1] = 0;
     }
 
     line(v1, v2, c) {
         const line = this.group.getObjectByName('lines');
-        let i = this.sizes[2] * 2;
+        let i = this.indices[2] * 2;
         const position = line.geometry.attributes.position;
         const color = line.geometry.attributes.color;
         position.setXYZ(i, v1.x, v1.y, v1.z);
@@ -151,11 +151,11 @@ export class ThreeDebugDraw extends DebugDraw {
         i += 1;
         position.setXYZ(i, v2.x, v2.y, v2.z);
         color.setXYZ(i, c.x, c.y, c.z);
-        this.sizes[2] += 1;
-        if (this.sizes[2] !== bufferSize) {
+        this.indices[2] += 1;
+        if (this.indices[2] !== bufferSize) {
             return;
         }
-        this.sizes[2] = 0;
+        this.indices[2] = 0;
     }
 
     render() {
@@ -167,6 +167,6 @@ export class ThreeDebugDraw extends DebugDraw {
                 normal.needsUpdate = true;
             }
         }
-        this.sizes = [0, 0, 0];
+        this.indices = [0, 0, 0];
     }
 }
